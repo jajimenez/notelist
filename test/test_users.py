b17fe587252c5d4f351e1f96dc36373f47bbabb2
Jose A. Jimenez <jajimenezcarm@gmail.com>
2021-04-01 01:05:51 +0200

    Implemented Login and Token Refresh unit tests

--- test/test_users.py
+++ test/test_users.py
@@ -7,7 +7,7 @@ import common
 class LoginTestCase(common.BaseTestCase):
     """Login resource unit tests."""
 
-    def test_post_ok(self):
+    def test_post(self):
         """Test the Get method of the Login resource.
 
         This test tries to log in as an existing user with valid credentials,
@@ -69,10 +69,10 @@ class LoginTestCase(common.BaseTestCase):
         # Check status code
         self.assertEqual(r.status_code, 401)
 
-    def test_post_wrong_password(self):
+    def test_post_invalid_password(self):
         """Test the Get method of the Login resource.
 
-        This test tries to log in as an existing user providing a wrong
+        This test tries to log in as an existing user providing an invalid
         password, which shouldn't work.
         """
         # Log in as the "admin" user
@@ -83,6 +83,128 @@ class LoginTestCase(common.BaseTestCase):
 
         # Check status code
         self.assertEqual(r.status_code, 401)
+
+    def test_get(self):
+        """Test the Get method of the Login resource.
+
+        This test tries to call the Get method, which shouldn't work.
+        """
+        r = self.client.get("/login")
+
+        # Check status code
+        self.assertEqual(r.status_code, 405)
+
+    def test_put(self):
+        """Test the Put method of the Login resource.
+
+        This test tries to call the Put method, which shouldn't work.
+        """
+        r = self.client.put("/login")
+
+        # Check status code
+        self.assertEqual(r.status_code, 405)
+
+    def test_delete(self):
+        """Test the Delete method of the Login resource.
+
+        This test tries to call the Delete method, which shouldn't work.
+        """
+        r = self.client.delete("/login")
+
+        # Check status code
+        self.assertEqual(r.status_code, 405)
+
+
+class TokenRefreshTestCase(common.BaseTestCase):
+    """Token Refresh resource unit tests."""
+
+    def test_post(self):
+        """Test the Post method of the Token Refresh resource.
+
+        This test tries to log in as an existing user and get a new, not fresh,
+        access token given the user refresh token, which should work.
+        """
+        # Log in as the "admin" user
+        data = {
+            "username": self.admin_username, "password": self.admin_password}
+        r = self.client.post("/login", json=data)
+
+        # Check status code
+        self.assertEqual(r.status_code, 200)
+
+        # Check result
+        self.assertIn("result", r.json)
+        result = r.json["result"]
+        self.assertEqual(type(result), dict)
+
+        # Check refresh token
+        self.assertIn("refresh_token", result)
+        refresh_token = result["refresh_token"]
+        self.assertEqual(type(refresh_token), str)
+        self.assertNotEqual(refresh_token, "")
+
+        # Get a new, not fresh, access token
+        headers = {"Authorization": f"Bearer {refresh_token}"}
+        r = self.client.post("refresh", headers=headers)
+
+        # Check status code
+        self.assertEqual(r.status_code, 200)
+
+        # Check result
+        self.assertIn("result", r.json)
+        result = r.json["result"]
+        self.assertEqual(type(result), dict)
+
+        # Check new access token
+        self.assertIn("access_token", result)
+        access_token = result["access_token"]
+        self.assertEqual(type(access_token), str)
+        self.assertNotEqual(access_token, "")
+
+    def test_post_missing_refresh_token(self):
+        """Test the Post method of the Token Refresh resource.
+
+        This test tries to get a new, not fresh, access token without providing
+        a refresh token, which shouldn't work.
+        """
+        # Get access token
+        r = self.client.post("refresh")
+
+        # Check status code
+        self.assertEqual(r.status_code, 401)
+
+    def test_post_invalid_refresh_token(self):
+        """Test the Post method of the Token Refresh resource.
+
+        This test tries to log in as an existing user and get a new, not fresh,
+        access token given a wrong refresh token, which shouldn't work.
+        """
+        # Log in as the "admin" user
+        data = {
+            "username": self.admin_username, "password": self.admin_password}
+        r = self.client.post("/login", json=data)
+
+        # Check status code
+        self.assertEqual(r.status_code, 200)
+
+        # Check result
+        self.assertIn("result", r.json)
+        result = r.json["result"]
+        self.assertEqual(type(result), dict)
+
+        # Check access token
+        self.assertIn("access_token", result)
+        access_token = result["access_token"]
+        self.assertEqual(type(access_token), str)
+        self.assertNotEqual(access_token, "")
+
+        # Get a new, not fresh, access token providing the access token instead
+        # of the refresh token.
+        headers = {"Authorization": f"Bearer {access_token}"}
+        r = self.client.post("refresh", headers=headers)
+
+        # Check status code
+        self.assertEqual(r.status_code, 422)
 
 
 if __name__ == "__main__":
