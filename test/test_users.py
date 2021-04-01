8e97dfd4c0ee36e9cc5d0b9b0776b40f8d748f55
Jose A. Jimenez <jajimenezcarm@gmail.com>
2021-04-01 16:23:37 +0200

    Implemented Logout unit tests

--- test/test_users.py
+++ test/test_users.py
@@ -177,7 +177,7 @@ class TokenRefreshTestCase(common.BaseTestCase):
         """Test the Post method of the Token Refresh resource.
 
         This test tries to log in as an existing user and get a new, not fresh,
-        access token given a wrong refresh token, which shouldn't work.
+        access token given an invalid refresh token, which shouldn't work.
         """
         # Log in as the "admin" user
         data = {
@@ -235,6 +235,86 @@ class TokenRefreshTestCase(common.BaseTestCase):
 
         # Check status code
         self.assertEqual(r.status_code, 405)
+
+
+class LogoutTestCase(common.BaseTestCase):
+    """Logout resource unit tests."""
+
+    def test_post(self):
+        """Test the Get method of the Logout resource.
+
+        This test tries to log in as an existing user with valid credentials
+        and then log out, which should work.
+        """
+        # Log in as the "admin" user
+        data = {
+            "username": self.admin_username, "password": self.admin_password}
+        r = self.client.post("/login", json=data)
+
+        # Check status code
+        self.assertEqual(r.status_code, 200)
+
+        # Check result
+        self.assertIn("result", r.json)
+        result = r.json["result"]
+        self.assertEqual(type(result), dict)
+
+        # Check access token
+        self.assertIn("access_token", result)
+        access_token = result["access_token"]
+        self.assertEqual(type(access_token), str)
+        self.assertNotEqual(access_token, "")
+
+        # Log out
+        headers = {"Authorization": f"Bearer {access_token}"}
+        r = self.client.post("/logout", headers=headers)
+
+        # Check status code
+        self.assertEqual(r.status_code, 200)
+
+    def test_post_missing_access_token(self):
+        """Test the Post method of the Logout resource.
+
+        This test tries to log out without providing an access token, which
+        shouldn't work.
+        """
+        # Logout
+        r = self.client.post("/logout")
+
+        # Check status code
+        self.assertEqual(r.status_code, 401)
+
+    def test_post_invalid_access_token(self):
+        """Test the Post method of the Logout resource.
+
+        This test tries to log out given an invalid access token, which
+        shouldn't work.
+        """
+        # Log in as the "admin" user
+        data = {
+            "username": self.admin_username, "password": self.admin_password}
+        r = self.client.post("/login", json=data)
+
+        # Check status code
+        self.assertEqual(r.status_code, 200)
+
+        # Check result
+        self.assertIn("result", r.json)
+        result = r.json["result"]
+        self.assertEqual(type(result), dict)
+
+        # Check access token
+        self.assertIn("access_token", result)
+        access_token = result["access_token"]
+        self.assertEqual(type(access_token), str)
+        self.assertNotEqual(access_token, "")
+
+        # Log out providing an invalid access token
+        headers = {"Authorization": f"Bearer {access_token + '_'}"}
+        r = self.client.post("/refresh", headers=headers)
+
+        # Check status code
+        self.assertEqual(r.status_code, 422)
 
 
 if __name__ == "__main__":
