c780bb7f07b25623b96749147e94b3559acfc658
Jose A. Jimenez <jajimenezcarm@gmail.com>
2021-04-19 21:01:42 +0200

    Fixed file name

---
+++ src/notelist/schemas/notebooks.py
@@ -0,0 +1,15 @@
+"""Module with the notebook schemas."""
+
+from notelist.ma import ma
+from notelist.models.notebooks import Notebook
+
+
+class NotebookSchema(ma.SQLAlchemyAutoSchema):
+    """Notebook schema."""
+
+    class Meta:
+        """Notebook schema metadata."""
+
+        model = Notebook
+        ordered = True
+        load_instance = True
