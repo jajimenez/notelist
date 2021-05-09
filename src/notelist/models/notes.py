ab7586aa96d0df6e31bfba79eae74e585ed11831
Jose A. Jimenez <jajimenezcarm@gmail.com>
2021-05-09 19:43:18 +0200

    Updated Note model. Renamed file.

--- src/notelist/models/notes.py
+++ src/notelist/models/notes.py
@@ -1,5 +1,6 @@
 """Module with the database note models."""
 
+from typing import List, Optional
 from notelist.db import db
 
 
@@ -36,3 +37,76 @@ class Note(db.Model):
     tags = db.relationship(
         "Tag", secondary=note_tags, lazy="subquery",
         backref=db.backref("notes", lazy=True))
+
+    @classmethod
+    def get_by_filter(
+        cls, notebook_id: int, active: Optional[bool] = None,
+        tags: Optional[List[int]] = None, no_tags: bool = False,
+        order_by_last_mod: bool = False
+    ) -> List["Note"]:
+        """Return all the notes of a notebook by a filter.
+
+        :param notebook_id: Notebook ID.
+        :param active: State filter (include active notes or not active notes).
+        :param tags: Tags filter (include notes that has any of these tags).
+        This list contains tag IDs.
+        :param no_tags: Notes with No Tags filter (include notes with no tags).
+        This filter is only applicable if a tag filter has been provided, i.e.
+        `tags` is not None).
+        :param order_by_last_mod: `True` if notes should be sorted by their
+        last modification timestamp. `False` if notes should be sorted by their
+        creation timestamp (default).
+        :return: List of `Note` instances.
+        """
+        result = []
+        notes = cls.query.filter_by(notebook_id=notebook_id)
+
+        # State filter
+        if active:
+            notes = notes.filter_by(active=True)
+        elif active is not None:
+            notes = notes.filter_by(active=False)
+
+        # Order
+        if order_by_last_mod:
+            notes = notes.order_by(Note.last_modification_ts).all()
+        else:
+            notes = notes.order_by(Note.creation_ts).all()
+
+        # Tags and Not Tags filters
+        def select_note(n: "Note") -> bool:
+            """Return whether a note should be included or not based on its
+            tags.
+
+            :param n: Note.
+            :return: `True` if `n` should be included. `False` otherwise.
+            """
+            if no_tags and len(n.tags) == 0:
+                return True
+
+            note_tags = map(lambda x: x.id, n.tags)
+            return any(map(lambda x: x in note_tags, tags))
+
+        if tags is not None:
+            result = filter(notes, select_note)
+
+        return result
+
+    @classmethod
+    def get(cls, _id: int) -> "Note":
+        """Return a note given its ID.
+
+        :param _id: Note ID.
+        :return: `Note` instance.
+        """
+        return cls.query.filter_by(id=_id).first()
+
+    def save(self):
+        """Save the note."""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """Delete the note."""
+        db.session.delete(self)
+        db.session.commit()
