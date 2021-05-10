d29d80015a5c1ab37437861da4549b3c9fc50062
Jose A. Jimenez <jajimenezcarm@gmail.com>
2021-05-11 00:51:48 +0200

    Model fix

--- src/notelist/models/notebooks.py
+++ src/notelist/models/notebooks.py
@@ -14,6 +14,8 @@ class Notebook(db.Model):
     name = db.Column(db.String(100), nullable=False)
     tags = db.relationship(
         "Tag", backref="notebook", cascade_backrefs="all, delete", lazy=True)
+    notes = db.relationship(
+        "Note", backref="notebook", cascade_backrefs="all, delete", lazy=True)
 
     # Constraint: 2 or more notebooks of the same user can't have the same name
     __table_args__ = (
